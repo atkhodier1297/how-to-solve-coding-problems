0a281d4cb0b349c5dbb8c717a4f5d45e024717eb
Adam Khodier <khodieradam1297@gmail.com>
2023-09-14 10:34:59 -0400

    first example must breakdown how to solve

---
+++ index.js
@@ -0,0 +1,12 @@
+// Given 2 arrays, create a function that let's a user know T/F 
+// whether these two arrays contain any common items.
+
+// EXAMPLE
+
+// const array1 = ['a', 'b', 'c', 'x']
+// const array2 = ['z', 'y', 'i']
+// should return False
+
+// const array3 = ['a', 'b', 'c', 'x']
+// const array4 = ['z', 'y', 'x']
+// should return true
