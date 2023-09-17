a4eba8495173d31411cd6d18aaa7a4cc9b9014eb
Adam Khodier <khodieradam1297@gmail.com>
2023-09-17 14:55:25 -0400

    set up for a faster solution

--- index.js
+++ index.js
@@ -21,16 +21,15 @@
 // may or may not wanna code the brute force solution
 // describe why brute force solution is not the best
 
-
 const array1 = ['a', 'b', 'c', 'x']
 const array2 = ['z', 'y', 'i']
 const array3 = ['a', 'b', 'c', 'x']
 const array4 = ['z', 'y', 'x']
 
-function containsCommonItem(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-            if (arr1[i] === arr2[j]) {
+function containsCommonItem(array1, array2) {
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (array1[i] === array2[j]) {
                 return true
             }
         }
@@ -38,8 +37,44 @@ function containsCommonItem(arr1, arr2) {
     return false
 }
 
-answer1 = containsCommonItem(array1, array2)
-console.log(answer1)
+// answer1 = containsCommonItem(array1, array2)
+// console.log(answer1)
 
-answer2 = containsCommonItem(array3, array4)
-console.log(answer2)
+// answer2 = containsCommonItem(array3, array4)
+// console.log(answer2)
+
+// since arrays are not the same length it would be O(a * b)
+// is there a way to turn this to a faster time complexity?
+
+// array1 ==> obj {
+    // a true
+    // b true
+    // c true
+    // x true
+//}
+// array2{index} === obj.properties
+// then you can only loop over the second array 
+// to compare it to the first array that is now obj
+// LESS NESTED LOOPS
+
+function commonItem2(array1, array2) {
+    // loop through first array and create obj 
+    // where properties === items in the array
+    let map = {}
+    for (let i = 0; i < array1.length; i++) {
+        if(!map[array1[i]]) {
+            const item = array1[i]
+            map[item] = true
+        }
+    }
+    console.log(map)
+    // loop through second array and check of item in
+    // 2nd array exists on created object
+    // we will have two loops but not nested
+}
+
+// O(a + b)
+// although it is not solved yet the interview sees how I think
+
+console.log(commonItem2(array1, array2))
+
